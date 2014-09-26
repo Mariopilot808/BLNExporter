ffaed6f31ff368cacbef65600de1c12cb5ad9b47
Mariopilot808 <mario@norcex.com>
2014-09-26 18:12:50 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,34 @@
+"""
+/*******BLNexp
+                                 A QGIS plugin
+ Exports to Vector data to SurferBLN
+                             -------------------
+        begin                : 2014-16-09
+        copyright            : (C) 2014 by Mario Noriega
+        email                : Mario@norcex.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+def name():
+    return "BLN Exporter"
+def description():
+    return "Converts Vector layers to BLN"
+def version():
+    return "Version 0.1"
+def icon():
+    return "icon.png"
+def qgisMinimumVersion():
+    return "2.0"
+def classFactory(iface):
+    # load BLNExp class from file BLNExp
+    from BLNExp import blnexp
+    return blnexp(iface)
